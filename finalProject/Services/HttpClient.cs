b4e4294b14211d94f27b7c66d62f10f075dabd57
Michael Spencer <m.spencer@elyoninternational.com>
2022-07-11 14:51:06 -0700

    first commit

---
+++ finalProject/Services/HttpClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using finalProject.Models;
+using finalProject.Services.Interfaces;
+using System.Net.Http;
+
+namespace finalProject.Services
+{
+    public class HttpClient : IHttpClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HttpClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IEnumerable<DataModel> entries { get; set; }
+
+        public async Task<DataModel> GetAsync()
+        {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://api.publicapis.org/");
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+        }
+    }
+}
